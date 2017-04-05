e7ffa932aaf7ca16086c859a1dd87bf236329383
An Ngo <ngoan_230@yahoo.com>
2017-04-04 21:00:12 -0400

    change font weight for text

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,10 +3,10 @@ class TasksController < ApplicationController
 
 	def index
 		if params[:category].blank?
-		@tasks = Task.all.order("created_at DESC")
-	else
-		@category_id = Category.find_by(name: params[:category]).id
-		@tasks = Task.where(category_id: @category_id).order("created_at DESC")
+			@tasks = Task.all.order("created_at DESC")
+		else
+			@category_id = Category.find_by(name: params[:category]).id
+			@tasks = Task.where(category_id: @category_id).order("created_at DESC")
 		end
 	end
 
